58d5b0869d100190ab65a57cde8a0db3ca539e9d
zhuoh <zhuohong@live.com>
2019-07-27 12:19:38 +0800

    fix bug

--- mep.go
+++ mep.go
@@ -8,14 +8,14 @@ import (
 func main() {
 	gli.Gli{
 		Name:    "Mep",
-		Version: "0.1beta",
+		Version: "0.2beta",
 		Sketch:  "Simple proxy for GWF",
 		Author:  "cheuk <zhuohong@live.com>",
 		Commands: gli.Commands{
-			gli.Command{
+			{
 				Name: "start",
 				Options: []gli.Option{
-					{"-addr", "0.0.0.0:1080", "specified listen ip"},
+					{"addr", "0.0.0.0:1080", "specified listen ip"},
 				},
 				Sketch: "Start socks4(a)/5 proxy server",
 				Action: func(cok gli.Cok) error {
